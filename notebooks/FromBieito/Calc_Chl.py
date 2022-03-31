a819b4b731944ea3b5f354e1f2c970a0852099cc
Clara Douglas <ccd1n18@soton.ac.uk>
2022-03-31 17:08:47 +0100

    Modified function file for calc int chl... still not functional tho

--- notebooks/FromBieito/Calc_Chl.py
+++ notebooks/FromBieito/Calc_Chl.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import glob
 import seawater as sw
-from soccom_functions import *
+#from soccom_functions import *
+from FromBieito.soccom_functions import *
 import pickle 
 import datetime
 import xarray as xr
@@ -12,8 +13,6 @@ import plotly.graph_objs as go
 import cmocean
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
-%matplotlib inline 
-#!pip install PyAstronomy
 from PyAstronomy import pyasl
 
 # calculate the vertically integrated data column inventory using the composite trapezoidal rule
@@ -29,5 +28,164 @@ def integrate(zi, data, depth_range):
     return col_inv
 
 
-class austral_chl_calcs():
-    def __init__(self,
+# calculate the vertically integrated data column inventory using the composite trapezoidal rule
+def integrate_wMLD(zi, data, start_depth, MLD):
+    #n_profs = len(data)
+    zi_start = abs(zi - start_depth).argmin() # find index of start depth
+    zi_end =  abs(zi - MLD).argmin() # find index of end depth
+    zi_struct = zi[zi_start : zi_end] # add +1 to get the 200m value
+    data = data[zi_start : zi_end] # add +1 to get the 200m value
+    col_inv = []
+    #for n in range(0, len(data)):
+    col_inv.append(np.trapz(data[:][~np.isnan(data[:])], zi_struct[:][~np.isnan(data[:])]))
+    return col_inv
+
+def integrate_chl(FLOAT):
+    # Interpolate/get data set up
+    zi = np.arange(0, 2005, 5) # 5 = 400 depth intervals between 0m to 2000m
+    #or zi=self.gr["depth"] #which = np.arange(0,2000+dz,dz)
+    chl_int = FLOAT.gr["Chl_a"] # interpolated chl values across zi depth intervals for all profiles
+    
+    ### Using a fixed lower limit
+    # Integrate chl - total chl in upper 200m
+    upperlim=0
+    lowerlim=200
+    AllFloats[FLOAT.gr["WMO_code"]]['chl_intr200'] = np.array(integrate(zi, chl_int.T, [upperlim, lowerlim])) # integrate interpolated nitrate values between 0m-200m 
+    
+    ### Using the deepest MLD seen by the float
+    # Integrate chl - total chl in upper ML - to deepest MLD seen by float
+    upperlim=0
+    lowerlim=np.nanmax(FLOAT.gr["mld"])
+    print(FLOAT.gr["WMO_code"],"Deepest mixed layer: %1.0f m"%(lowerlim))
+    AllFloats[FLOAT.gr["WMO_code"]]['chl_intr_dMLD'] = np.array(integrate(zi, chl_int.T, [upperlim, lowerlim])) # integrate interpolated nitrate values between 0m-200m 
+
+    ### Using the profile specific MLD
+    H2=FLOAT.gr["mld"]
+    nz= len(H2)
+    # using MLD calculated for each individual profile
+    chl_T=chl_int.T # the interpolated chl from FLOAT.gr
+    AllFloats[FLOAT.gr["WMO_code"]]['dint_chla'] = np.full( nz, np.nan )
+    for i in range(nz): #nz = length H2 (mld array) = number of profiles
+        if np.isnan(H2[i]):
+            AllFloats[FLOAT.gr["WMO_code"]]['dint_chla'][i] = np.nan # in float used to write code, there were some nan for MLD because of no salinity values recorded
+        else:
+            lowerlim=H2[i]
+            data=chl_T[i,:]
+            AllFloats[FLOAT.gr["WMO_code"]]['dint_chla'][i] = np.array(integrate_wMLD(zi, data, upperlim, lowerlim)) # integrate interpolated nitrate values between 0m-specificMLD
+    
+    ### The entire water column
+    upperlim=0
+    lowerlim=2000
+    AllFloats[FLOAT.gr["WMO_code"]]['chl_intrWC'] = np.array(integrate(zi, chl_int.T, [upperlim, lowerlim])) # integrate interpolated nitrate values between 0m-200m 
+    
+    return AllFloats[FLOAT.gr["WMO_code"]]['chl_intr200'],AllFloats[FLOAT.gr["WMO_code"]]['chl_intr_dMLD'],AllFloats[FLOAT.gr["WMO_code"]]['dint_chla'],AllFloats[FLOAT.gr["WMO_code"]]['chl_intrWC']
+    #or return AllFloats                                      
+    
+def chl_underornoice(data,yix):
+    H2=FLOAT.gr["mld"]
+    nz= len(H2)
+    ## How much of the annual chl was present under ice?
+    AllFloats[FLOAT.gr["WMO_code"]]['underice_yesno']=np.full(nz, np.nan)
+    for i in range(nz):
+        test=np.isnan(FLOAT.gr["Chl_a"][0,i]) & np.isnan(FLOAT.gr["Chl_a"][1,i]) & np.isnan(FLOAT.gr["Chl_a"][2,i]) #& np.isnan(FLOAT.gr["Chl_a"][3,i]) #& np.isnan(FLOAT.gr["Chl_a"][4,i]) #& np.isnan(FLOAT.gr["Chl_a"][5,i])
+        temp_thres=FLOAT.gr["Temperature"][0:4,i]<(-1.78)
+        if test:
+            AllFloats[FLOAT.gr["WMO_code"]]['underice_yesno'][i]=1
+        else:
+            AllFloats[FLOAT.gr["WMO_code"]]['underice_yesno'][i]=0
+    #### Over the whole float timeseries
+    ui=np.where(np.isnan(FLOAT.gr["Chl_a"][0,:]) & np.isnan(FLOAT.gr["Chl_a"][1,:]) & np.isnan(FLOAT.gr["Chl_a"][2,:]) & np.isnan(FLOAT.gr["Chl_a"][3,:])) #& np.isnan(FLOAT.gr["Chl_a"][4,i]) #& np.isnan(FLOAT.gr["Chl_a"][5,i])
+    ui_tot=np.nansum(AllFloats[FLOAT.gr["WMO_code"]]['chl_intr_dMLD'][ui])
+    nui=np.where(np.isfinite(FLOAT.gr["Chl_a"][0,:]) & np.isnan(FLOAT.gr["Chl_a"][1,:]) & np.isnan(FLOAT.gr["Chl_a"][2,:]) & np.isnan(FLOAT.gr["Chl_a"][3,:])) #& np.isnan(FLOAT.gr["Chl_a"][4,i]) #& np.isnan(FLOAT.gr["Chl_a"][5,i])
+    nui_tot=np.nansum(AllFloats[FLOAT.gr["WMO_code"]]['chl_intr_dMLD'][test])
+    #print(np.nansum(chl_intr_dMLD))
+    #print('approx % of chl under ice during float timeseries:', round(ui_tot/nui_tot*100,2),'%')
+    
+    #### get index for profiles that are in austral year, under ice and ice free
+    AllFloats[FLOAT.gr["WMO_code"]][yix]["aust_ix"]=[]
+    AllFloats[FLOAT.gr["WMO_code"]][yix]["underice_ix"]=[]
+    AllFloats[FLOAT.gr["WMO_code"]][yix]["icefree_ix"]=[]
+    for i in range(len(FLOAT.gr["date_dt"])):
+        decy = pyasl.decimalYear(FLOAT.gr["date_dt"][i]) #might be more efficient to make decy an np.int_ here so all the logical queries can be called at once?...
+        smol=decy>=(yix-0.5)
+        tol=decy<(yix+0.5)
+        underice=AllFloats[FLOAT.gr["WMO_code"]]['underice_yesno'][i]>0
+        icefree=AllFloats[FLOAT.gr["WMO_code"]]['underice_yesno'][i]==0
+        if smol==tol:
+            AllFloats[FLOAT.gr["WMO_code"]][yix]["aust_ix"]=np.append(AllFloats[FLOAT.gr["WMO_code"]][yix]["aust_ix"],np.int_(i))
+        if smol==tol==underice:
+            AllFloats[FLOAT.gr["WMO_code"]][yix]["underice_ix"]=np.append(AllFloats[FLOAT.gr["WMO_code"]][yix]["underice_ix"],i)
+        if smol==tol==icefree:
+            AllFloats[FLOAT.gr["WMO_code"]][yix]["icefree_ix"]=np.append(AllFloats[FLOAT.gr["WMO_code"]][yix]["icefree_ix"],i)
+    AllFloats[FLOAT.gr["WMO_code"]][yix]["aust_ix"]=np.int_(AllFloats[FLOAT.gr["WMO_code"]][yix]["aust_ix"])
+    AllFloats[FLOAT.gr["WMO_code"]][yix]["underice_ix"]=np.int_(AllFloats[FLOAT.gr["WMO_code"]][yix]["underice_ix"])
+    AllFloats[FLOAT.gr["WMO_code"]][yix]["icefree_ix"]=np.int_(AllFloats[FLOAT.gr["WMO_code"]][yix]["icefree_ix"])
+    # pos exlude years with <34 profiles from next lot of calculations?
+    ### Calculate how much Chl is present at specific times - whole year, under ice, when there's no ice
+    #could add if rule: if len(AllFloats['5904472'][endyear]["aust_ix"])<34: continue... or [] =np.nan
+    #### Total in austral year
+    AllFloats[FLOAT.gr["WMO_code"]][yix]["Tot_aus_chl"]=np.round(np.nansum(AllFloats[FLOAT.gr["WMO_code"]]['chl_intr200'][AllFloats[FLOAT.gr["WMO_code"]][yix]["aust_ix"]]),6)
+    #### Total under ice
+    AllFloats[FLOAT.gr["WMO_code"]][yix]["Tot_underice_chl"]=np.round(np.nansum(AllFloats[FLOAT.gr["WMO_code"]]['chl_intr200'][AllFloats[FLOAT.gr["WMO_code"]][yix]["underice_ix"]]),6)
+    #### Total when ice-free
+    AllFloats[FLOAT.gr["WMO_code"]][yix]["Tot_icefree_chl"]=np.round(np.nansum(AllFloats[FLOAT.gr["WMO_code"]]['chl_intr200'][AllFloats[FLOAT.gr["WMO_code"]][yix]["icefree_ix"]]),6)
+    #### proportion under ice for austral year
+    AllFloats[FLOAT.gr["WMO_code"]][yix]["Prop_underice"]=AllFloats[FLOAT.gr["WMO_code"]][yix]["Tot_underice_chl"]/AllFloats[FLOAT.gr["WMO_code"]][yix]["Tot_aus_chl"]
+    AllFloats[FLOAT.gr["WMO_code"]][yix]["Prop_icefree"]=AllFloats[FLOAT.gr["WMO_code"]][yix]["Tot_icefree_chl"]/AllFloats[FLOAT.gr["WMO_code"]][yix]["Tot_aus_chl"]
+    #### Mean [Chl] in WC
+    #AllFloats['5904472'][yix]["Mean_aus_chl"]=np.round(np.nanmean(chl_intrWC[AllFloats['5904472'][yix]["aust_ix"]]),6)
+    #for yix in range(startyear,endyear+1,1):
+    print(yix,"under ice",np.round(AllFloats[FLOAT.gr["WMO_code"]][yix]["Prop_underice"],3))
+    #for yix in range(startyear,endyear+1,1):
+    #    print(yix,"ice free",np.round(AllFloats['5904472'][yix]["Prop_icefree"],3))
+    
+    #return AllFloats[FLOAT.gr["WMO_code"]]['underice_yesno'],AllFloats[FLOAT.gr["WMO_code"]][yix]["aust_ix"],AllFloats[FLOAT.gr["WMO_code"]][yix]["underice_ix"],AllFloats[FLOAT.gr["WMO_code"]][yix]["icefree_ix"],AllFloats[FLOAT.gr["WMO_code"]][yix]["Tot_aus_chl"],AllFloats[FLOAT.gr["WMO_code"]][yix]["Tot_underice_chl"],AllFloats[FLOAT.gr["WMO_code"]][yix]["Tot_icefree_chl"],AllFloats[FLOAT.gr["WMO_code"]][yix]["Prop_underice"],AllFloats[FLOAT.gr["WMO_code"]][yix]["Prop_icefree"]
+    return AllFloats
+
+
+def nosat_chl(data,yix):
+    ## What is happening to the chl when the satellite isn't seeing the ice-free waters (from ~mid March)?
+    Mar3015=datetime.datetime(2015,3,30)
+    decmar15=pyasl.decimalYear(Mar3015)
+    Mar2116=datetime.datetime(2016,3,21)
+    decmar16=pyasl.decimalYear(Mar2116)
+    Mar2217=datetime.datetime(2017,3,22)
+    decmar17=pyasl.decimalYear(Mar2217)
+    Mar1418=datetime.datetime(2018,3,14)
+    decmar18=pyasl.decimalYear(Mar1418)
+    Mar1419=datetime.datetime(2019,3,14)
+    decmar19=pyasl.decimalYear(Mar1419)
+    Mar2120=datetime.datetime(2020,3,21)
+    decmar20=pyasl.decimalYear(Mar2120)
+
+    AllFloats[FLOAT.gr["WMO_code"]][yix]["no_sat"]=[]
+    for i in range(len(FLOAT.gr["date_dt"])):
+        decy = pyasl.decimalYear(FLOAT.gr["date_dt"][i])
+        smol=decy>=(yix-0.5)
+        tol=decy<(yix+0.5)
+        icefree=AllFloats[FLOAT.gr["WMO_code"]]['underice_yesno'][i]==0
+        if yix==2015:
+            mar=decy>decmar15
+        elif yix==2016:
+            mar=decy>decmar16
+        elif yix==2017:
+            mar=decy>decmar17
+        elif yix==2018:
+            mar=decy>decmar18
+        elif yix==2019:
+            mar=decy>decmar19
+        elif yix==2020:
+            mar=decy>decmar20
+        elif yix==2021:
+            continue
+        if smol==tol==icefree==mar:
+            AllFloats[FLOAT.gr["WMO_code"]][yix]["no_sat"]=np.append(AllFloats[FLOAT.gr["WMO_code"]][yix]["no_sat"],i)
+        AllFloats[FLOAT.gr["WMO_code"]][yix]["no_sat"]=np.int_(AllFloats[FLOAT.gr["WMO_code"]][yix]["no_sat"])
+    
+    #could add if rule: if len(AllFloats['5904472'][endyear]["aust_ix"])<34: continue... or [] =np.nan
+    #### Total chl when satellite field of view reduces during March, but before ice returns
+    AllFloats[FLOAT.gr["WMO_code"]][yix]["Tot_nosat_chl"]=np.round(np.nansum(AllFloats[FLOAT.gr["WMO_code"]]['chl_intr200'][AllFloats[FLOAT.gr["WMO_code"]][yix]["no_sat"]]),6)
+    #### proportion under ice for austral year
+    AllFloats[FLOAT.gr["WMO_code"]][yix]["Prop_nosat"]=AllFloats[FLOAT.gr["WMO_code"]][yix]["Tot_nosat_chl"]/AllFloats[FLOAT.gr["WMO_code"]][yix]["Tot_aus_chl"]
+    print(yix,"no satellite coverage",np.round(AllFloats[FLOAT.gr["WMO_code"]][yix]["Prop_nosat"],3))
+    return AllFloats
